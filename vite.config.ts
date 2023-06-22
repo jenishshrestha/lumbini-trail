c56dff04b4192e39fd05e7033c82eb4de0c00b74
sriza <sriza054@gmail.com>
2023-06-22 10:59:25 +0545

    map api updated

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
-    port: 3000, // you can replace this port with any port
+    port: 3005, // you can replace this port with any port
   },
   define: {
     'process.env': {
